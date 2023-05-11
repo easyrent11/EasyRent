76184a0bc548d3e908da64deba3e472d6c7968d9
Sobhi <omar_sobhi_finalproject12@outlook.com>
2023-05-12 00:05:45 +0300

    Fully created the login and register components!

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  // use state for the username and password.
+  const [username,setUsername] = useState("");
+  const [password,setPassword] = useState("");
+  const [isOpen, setIsOpen] = useState(true);
+  const navigate = useNavigate();
+
+  const handleClose = () => {
+    setIsOpen(false);
+    navigate('/');
+  };
+
+  if (!isOpen) {
+    return null;
+  }
+
   return (
     <>
-       <form>Login
-      <input type='text' name='aaa'/>
+      <section className="fixed inset-0 flex items-center justify-center z-50 bg-opacity-50 bg-gray-900">
+        <form className="bg-[#E7E7E7] max-w-lg p-6 mx-auto rounded-lg h-2/4">
+          <div className="flex justify-end">
+            <button
+              type="button"
+              className="bg-gray-300 text-gray-700 px-4 py-2 rounded-lg"
+              onClick={handleClose}
+            >
+              X
+            </button>
+          </div>
+          <h2 className="text-2xl font-bold text-center mb-4">Login</h2>
 
-       </form>
-        <p>OOOOOOOOOOOOOOO</p>
+          <label className="mb-2">Username</label>
+          <input type="text" name='username' className="border border-gray-300 px-4 py-2 mt-4 mb-4 rounded-md w-full" onChange={(e) => {setUsername(e.target.value)}} />
+
+          <label className="mt-4 mb-2">Password</label>
+          <input type="password" name='password' className="border border-gray-300 px-4 py-2 mb-4 mt-1 rounded-md w-full"  onChange={(e) => {setPassword(e.target.value)}}/>
+
+          <button
+            type="submit"
+            className="bg-[#CC6200] text-white px-4 py-2 rounded-md w-full mt-7"
+          >
+            Login
+          </button>
+        
+          <p className="text-center mt-10">
+          Forgot your password?
+          <Link to="/login" className="p-2 text-[#CC6200]">
+            Reset
+          </Link>
+          </p>
+        </form>
+      </section>
     </>
- 
-  )
+  );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,137 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Register() {
+  // use state for all user credentials.
+  // const [] = useState("");
+  // const [] = useState("");
+  // const [] = useState("");
+  // const [] = useState("");
+  // const [] = useState("");
+  // const [] = useState("");
+  // const [] = useState("");
+  // const [] = useState("");
+  // const [] = useState("");
+  // const [] = useState("");
+  // const [] = useState("");
+
+  const [isOpen, setIsOpen] = useState(true);
+  const navigate = useNavigate();
+
+  const handleClose = () => {
+    setIsOpen(false);
+    navigate('/');
+  };
+
+  if (!isOpen) {
+    return null;
+  }
+
   return (
-    <>
-      <form>
-        <input/>
-        <input type='text'/>
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-opacity-50 bg-gray-900">
+      <form className="bg-[#E7E7E7] max-w-lg p-6 mx-auto rounded-lg">
+      <div className="flex justify-end">
+          <button
+            type="button"
+            className="bg-gray-300 text-gray-700 px-4 py-2 rounded-lg"
+            onClick={handleClose}
+          >
+            X
+          </button>
+        </div>
+        <h2 className="text-2xl font-bold text-center mb-4">Register</h2>
+        <p className="text-center mb-4">
+          Already have an account?
+          <Link to="/login" className="p-2 text-[#CC6200]">
+            Log in
+          </Link>
+        </p>
+        <div className="grid grid-cols-2 gap-4 mb-4">
+          <div>
+            <label className="block mb-2">First Name</label>
+            <input
+              type="text"
+              className="border border-gray-300 px-4 py-2 rounded-md w-full"
+            />
+          </div>
+          <div>
+            <label className="block mb-2">Last Name</label>
+            <input
+              type="text"
+              className="border border-gray-300 px-4 py-2 rounded-md w-full"
+            />
+          </div>
+          <div>
+            <label className="block mb-2">Email Address</label>
+            <input
+              type="email"
+              className="border border-gray-300 px-4 py-2 rounded-md w-full"
+            />
+          </div>
+          <div>
+            <label className="block mb-2">Phone Number</label>
+            <input
+              type="tel"
+              className="border border-gray-300 px-4 py-2 rounded-md w-full"
+            />
+          </div>
+          <div>
+            <label className="block mb-2">Password</label>
+            <input
+              type="password"
+              className="border border-gray-300 px-4 py-2 rounded-md w-full"
+            />
+          </div>
+          <div>
+            <label className="block mb-2">Verify Password</label>
+            <input
+              type="password"
+              className="border border-gray-300 px-4 py-2 rounded-md w-full"
+            />
+          </div>
+          <div>
+            <label className="block mb-2">Choose a City</label>
+            <input
+              type="text"
+              className="border border-gray-300 px-4 py-2 rounded-md w-full"
+            />
+          </div>
+          <div>
+            <label className="block mb-2">Full Address</label>
+            <input
+              type="text"
+              className="border border-gray-300 px-4 py-2 rounded-md w-full"
+            />
+          </div>
+          <div>
+            <label className="block mb-2">Profile Picture</label>
+            <input
+              type="file"
+              className="border border-gray-300 px-4 py-2 rounded-md w-full"
+            />
+          </div>
+          <div>
+            <label className="block mb-2">Government ID</label>
+            <input
+              type="text"
+              className="border border-gray-300 px-4 py-2 rounded-md w-full"
+            />
+          </div>
+          <div>
+            <label className="block mb-2">Driving License</label>
+            <input
+              type="text"
+              className="border border-gray-300 px-4 py-2 rounded-md w-full"
+            />
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="bg-[#CC6200] text-white px-4 py-2 rounded-md w-full"
+        >
+          Register
+        </button>
       </form>
-    </>
-  )
+    </div>
+  );
 }
