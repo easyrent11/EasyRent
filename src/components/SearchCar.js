7a90f37fafc2ac6be3e8705d96a4a4039d3a6c65
Omar Taha <omar_sobhi_finalproject12@outlook.com>
2023-05-16 09:55:50 +0300

    Fixed layout

--- src/components/SearchCar.js
+++ src/components/SearchCar.js
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import Select from 'react-select';
-import { Cities } from '../res/Cities';
-import { CarTypes } from '../res/CarTypes';
+import React, { useState } from "react";
+import Select from "react-select";
+import { Cities } from "../res/Cities";
+import { CarTypes } from "../res/CarTypes";
 
 export default function SearchCarForm() {
-  const [location, setLocation] = useState('');
-  const [pickDate, setPickDate] = useState('');
-  const [returnDate, setReturnDate] = useState('');
-  const [carType, setCarType] = useState('');
-
+  const [location, setLocation] = useState("");
+  const [pickDate, setPickDate] = useState("");
+  const [returnDate, setReturnDate] = useState("");
+  const [carType, setCarType] = useState("");
 
   const handleLocationChange = (selectedOption) => {
     setLocation(selectedOption.value);
@@ -30,12 +29,12 @@ export default function SearchCarForm() {
     e.preventDefault();
     // Handle form submission
     // You can access the form values using the state variables (location, pickDate, returnDate, carType)
-    console.log('Form submitted:', location, pickDate, returnDate, carType);
+    console.log("Form submitted:", location, pickDate, returnDate, carType);
   };
 
   return (
-    <form className='flex item-center justify-center p-4 w-1/2 ' onSubmit={handleSubmit}>
-      <div className='flex-1  mr-2'>
+    <form className="grid grid-cols-4 gap-4 p-4 w-5/6" onSubmit={handleSubmit}>
+      <div className="col-span-2 md:col-span-1">
         <Select
           id="location"
           value={{ value: location, label: location }}
@@ -44,15 +43,27 @@ export default function SearchCarForm() {
         />
       </div>
 
-      <div className='flex items-center justify-center flex-1 mr-2 bg-white'>
-        <input type="date" id="pickDate" value={pickDate} onChange={handlePickDateChange} />
+      <div className="col-span-2 md:col-span-1">
+        <input
+          type="date"
+          id="pickDate"
+          value={pickDate}
+          onChange={handlePickDateChange}
+          className="w-full"
+        />
       </div>
 
-      <div className='flex items-center justify-center flex-1 mr-2 bg-white'>
-        <input type="date" id="returnDate" value={returnDate} onChange={handleReturnDateChange} />
+      <div className="col-span-2 md:col-span-1">
+        <input
+          type="date"
+          id="returnDate"
+          value={returnDate}
+          onChange={handleReturnDateChange}
+          className="w-full"
+        />
       </div>
 
-      <div className='flex-1 mr-2'>
+      <div className="col-span-2 md:col-span-1">
         <Select
           id="carType"
           value={{ value: carType, label: carType }}
@@ -61,8 +72,14 @@ export default function SearchCarForm() {
         />
       </div>
 
-      <button className='bg-black text-1xl text-white p-1 w-1/5 ' type="submit">Search</button>
+      <div className="col-span-2">
+        <button
+          className="bg-black text-1xl text-white mx-0 m-auto p-1 w-1/2 md:w-3/5"
+          type="submit"
+        >
+          Search
+        </button>
+      </div>
     </form>
   );
-};
-
+}
