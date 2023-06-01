c9ecf56c271a19c72de031621653ca77393d455c
Sobhi <omar_sobhi_finalproject12@outlook.com>
2023-06-02 02:34:10 +0300

    added authorization

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,14 @@
+import {useNavigate } from 'react-router-dom';
+import useAuthentication from '../components/userAuthentication';
+
+export default function PrivateRoute({ component: Component, ...rest }) {
+  const { token } = useAuthentication();
+  const navigate = useNavigate();
+
+  if (!token) {
+    navigate('/login'); // Redirect to the login page
+    return null; // Render nothing while navigating
+  }
+
+  return <Component {...rest} />;
+};
