9b4a89b84b307fd9adee5432a6d81d75a1e4fe4d
Sobhi <omar_sobhi_finalproject12@outlook.com>
2023-06-04 02:32:44 +0300

    rmoved .env

--- server/models/db.js
+++ server/models/db.js
@@ -1,19 +1,31 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-// defining the sql connection.
-const conn = mysql.createConnection({
+
+const conn = mysql.createPool({
+  connectionLimit:100,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
 });
-conn.connect((err) => {
-  if (err) {
-    console.log('An error occurred while trying to connect to the database');
-  } else {
-    console.log('Connected to MySQL server...');
-  }
-});
+
+// const mysql = require('mysql');
+// const conn = mysql.createConnection({
+//   connectionLimit:100,
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE
+// });
+
+// conn.connect((err) => {
+//   if(err)
+//     console.log(err);
+//     else{
+//       console.log("Conencted to MYSQL database ");
+//     }
+    
+// })
 
 module.exports = conn;
