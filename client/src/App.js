5fdf39f1c2edf6790b47d52ee81f89176a3a3e65
Sobhi <omar_sobhi_finalproject12@outlook.com>
2023-06-10 17:58:01 +0300

    Working on the user page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState} from "react";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import CarView from "./components/CarView";
@@ -9,52 +9,83 @@ import FAQ from "./components/FAQ";
 import ContactUs from "./components/ContactUs";
 import Footer from "./components/Footer";
 import { CarListContext } from "./contexts/CarListContext";
+import { AllCarsContext } from "./contexts/AllCarsContext";
 import SearchResultDisplay from "./pages/SearchResultDisplay";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PrivateRoute from "./components/PrivateRoute";
 import UserLayout from "./pages/UserLayout";
-import UserNav from './components/UserNav';
+import UserNav from "./components/UserNav";
 import PageNotFound from "./components/PageNotFound";
 import AddCarForm from "./components/AddCar";
+import { getAllCars } from "./api/CarApi";
+import axios from "axios";
 
 function App() {
-
   const [carList, setCarList] = useState([]);
+  const [allCars, setAllCars] = useState([]);
+
   const [showLogin, setShowLogin] = useState(false);
   const [showRegister, setShowRegister] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [notFound,setNotFound] = useState(false);
+  const [notFound, setNotFound] = useState(false);
+  const [userProfileImage, setUserProfileImage] = useState(null);
 
   const updateCarList = (updatedList) => setCarList(updatedList);
-  const closeLogin = () =>  setShowLogin(false);
+  const setUserImage = (userImageUrl) => setUserProfileImage(userImageUrl);
+  const closeLogin = () => setShowLogin(false);
   const closeRegister = () => setShowRegister(false);
-  
+
+
+
   const openLogin = () => {
     setShowLogin(true);
     setShowRegister(false);
   };
 
   const openRegister = () => {
-
     setShowRegister(true);
     setShowLogin(false);
   };
 
-
-  const handleLogout = () =>{
+  const handleLogout = () => {
     setIsLoggedIn(false);
-  } 
+  };
 
   const handleLogin = () => setIsLoggedIn(true);
   const handleNotFound = () => setNotFound(true);
 
+  
+
+
+  useEffect(() => {
+    // Fetch all cars from backend API using Axios
+    axios
+      .get("http://localhost:3001/cars/getallcars")
+      .then((response) => {
+        console.log(response.data);
+        setAllCars(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching cars:", error);
+      });
+  }, []);
+
+  // try to get the token if the token exists change the isLogged state.
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) setIsLoggedIn(true);
+    else {
+      setIsLoggedIn(false);
+    }
+  }, []);
 
   return (
     <>
       <CarListContext.Provider value={{ carList, updateCarList }}>
+        <AllCarsContext.Provider value={allCars}>
+
         <Router>
-<<<<<<< HEAD
           {notFound ? (
             <PageNotFound handleNotFound={handleNotFound} />
           ) : isLoggedIn ? (
@@ -65,7 +96,6 @@ function App() {
           <Routes>
             <Route path="/" element={<HomeLayout />} />
             <Route path="/CarView/:platesNumber" element={<CarView />} />
-            <Route path="/Rating" element={<Rating />} />
             <Route path="/FAQ" element={<FAQ />} />
             <Route path="/ContactUs" element={<ContactUs />} />
             <Route path="/AddCar" element={<AddCarForm />} />
@@ -75,12 +105,7 @@ function App() {
               element={<SearchResultDisplay />}
             />
             {/* Private Home route for the logged in users */}
-            <Route
-              path="/user/homepage"
-              element={
-                <PrivateRoute openLogin={openLogin} component={UserLayout} />
-              }
-            />
+            <Route path="/homepage" element={<PrivateRoute openLogin={openLogin} component={UserLayout} />} />
             {/* catch all */}
             <Route
               path="*"
@@ -88,39 +113,16 @@ function App() {
             />
           </Routes>
 
-=======
-        {notFound ? (
-          <PageNotFound handleNotFound={handleNotFound} />
-        ) : isLoggedIn ? (
-          <UserNav handleLogout={handleLogout} />
-        ) : (
-          <NavBar openLogin={openLogin} openRegister={openRegister} />
-        )}
-            <Routes>
-              <Route path="/" element={<HomeLayout />} />
-              <Route path="/CarView/:platesNumber" element={<CarView />} />
-              <Route path="/FAQ" element={<FAQ />} />
-              <Route path="/ContactUs" element={<ContactUs isLoggedIn={isLoggedIn}/>} />
-              <Route
-                path="/SearchResultDisplay"
-                element={<SearchResultDisplay />}
-              />
-              {/* Private Home route for the logged in users */}
-              <Route path="/homepage" element={<PrivateRoute openLogin={openLogin} component={UserLayout} />} />
-              {/* catch all */}
-            <Route path="*" element={<PageNotFound handleNotFound={handleNotFound}/>} />
-            </Routes>
-        
->>>>>>> 4bd80dce2f0bef7879f49538336e09dfc7e5af17
           {/* conditional rendering login and register components. */}
           {showLogin && (
             <Login handleLogin={handleLogin} onClose={closeLogin} />
           )}
           {showRegister && (
-            <Register onClose={closeRegister} openLogin={openLogin} />
+            <Register onClose={closeRegister} openLogin={openLogin} setUserImage={setUserImage} />
           )}
           <Footer />
         </Router>
+        </AllCarsContext.Provider>
       </CarListContext.Provider>
       <ToastContainer />
     </>
